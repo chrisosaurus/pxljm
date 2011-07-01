f3f803ac315705243179b54ba8f99aa692e0c4c1
TrueShiftBlue <TrueShiftBlue@gmail.com>
2011-07-01 19:57:42 +1200

    Alter Game header

--- client/game/game.hpp
+++ client/game/game.hpp
@@ -7,16 +7,16 @@ class ClientGame {
         /**
          * Send a fleet of the specified amount from the source planet, heading towards a destination planet.
          */
-        void launchFleet(Planet &src, Planet &dest, int count);
+        void launch_fleet(Planet &src, Planet &dest, int count);
 
         /**
          * Find a planet based on id
          */
-        Planet *planetFromId(int id);
+        Planet *planet_from_id(int id);
 
         /**
          * Find the closest planet to a specified location.
          * If limit is specified, do not return a Planet further away than the squareroot of this.
          */
-        Planet *findNearestPlanet(int x, int y, int limit = 0);
+        Planet *find_nearest_planet(int x, int y, int limit = 0);
 };
