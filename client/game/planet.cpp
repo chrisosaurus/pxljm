d0c6ecab6c4cf866ec89d5ee91def9216ebb2f85
Dominik Schmid <DominikSchmid93@gmail.com>
2011-07-02 04:18:29 +1200

    added get_radius() to plant.cpp =p

--- client/game/planet.cpp
+++ client/game/planet.cpp
@@ -5,4 +5,5 @@ Planet::Planet(int vx, int vy, int vradius, int vid) : x(vx), y(vx), radius(50),
 int Planet::get_x() const { return x; }
 int Planet::get_y() const { return y; }
 
+int Planet::get_radius() const { return radius; }
 
