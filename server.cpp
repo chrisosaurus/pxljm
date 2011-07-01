836a6fbf78436b6d1e36b4d40baff63fa1fb3c05
Thomas Phillips <tntexplosivesltd@yahoo.co.nz>
2011-07-01 20:06:23 +1200

    Added cpp for server. Yay\!

---
+++ server.cpp
@@ -0,0 +1,47 @@
+#include "server.hpp"
+
+Server::Server(const unsigned int &listening_port, const int &num_clients)
+  : port(listening_port),
+  clients(num_clients)
+{
+}
+
+Server::get_error()
+{
+  return error;
+}
+
+Server::init()
+{
+  std::cout << "Trying to listen on port " << port << std::endl;
+  if (listener.Listen(port) != sf::Socket::Done)
+  {
+    std::cout << "Could not create socket" << std::endl;
+    error = "Could not create socket";
+    return false;
+  }
+  int clients_so_far;
+  std::cout << "Waiting for " << clients << "client(s)" << std::cout;
+  selector.Add(listener);
+
+  //while there still clients to connect
+  while (clients_so_far < clients)
+  {
+    if (selector.Wait())
+    {
+      if (selector.IsReady(listener))
+      {
+        // there is a client waiting to connect
+        sf::TcpSocket *client = new sf::TcpSocket;
+        if (listener.Accpet(*client) == sf::Socket::Done)
+        {
+          clients_so_far.push_back(client);
+          selector.Add(*client);
+          ++clients_so_far;
+          std::cout << clients_so_far << " have connected so far" << std::endl;
+        }
+      }
+    }
+  }
+  std::cout << "All clients have connected. Starting game..." << std::endl;
+}
