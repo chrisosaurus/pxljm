abc66bb7aaf836e92892e02ed94c69cf3b3c802a
Dominik Schmid <DominikSchmid93@gmail.com>
2011-07-02 04:14:47 +1200

    fixed constructor arguments. blarg.

--- client/game/planet.cpp
+++ client/game/planet.cpp
@@ -1,6 +1,6 @@
 #include "planet.hpp"
 
-Planet::Planet(int vx, int vy, int vradius, int vid) : x(X), y(Y), radius(Radius), id(id) { }
+Planet::Planet(int vx, int vy, int vradius, int vid) : x(vx), y(vx), radius(50), id(vid) { }
 
 int Planet::get_x() const { return x; }
 int Planet::get_y() const { return y; }
