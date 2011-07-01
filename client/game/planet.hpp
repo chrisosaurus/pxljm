14796758a9526e15f82073ee235cc17f59c07c42
TrueShiftBlue <TrueShiftBlue@gmail.com>
2011-07-01 19:55:56 +1200

    Alter Planet header

--- client/game/planet.hpp
+++ client/game/planet.hpp
@@ -4,13 +4,19 @@ public:
     
     ~Planet();
     
-    int getX() const;
+    int get_x() const;
 
-    int getY() const;
+    int get_y() const;
 
-    int getShipCount() const;
+    int get_ship_count() const;
 
-    void removeShips(int amount);
+    /**
+     * Removes ships (for instance, when this Planet is being attacked by a fleet)
+     * @param amount The amount of attacking ships to be processed at this instant
+     */
+    void remove_ships(int amount);
 
+    Fleet &launch_fleet();
+    
     virtual void logic();
 };
