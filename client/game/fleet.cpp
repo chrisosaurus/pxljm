bade1df5f0a714e842772729d4ec5d8054f20b49
Dominik Schmid <DominikSchmid93@gmail.com>
2011-07-02 03:24:54 +1200

    Brutally killed many types of those #include bugs

--- client/game/fleet.cpp
+++ client/game/fleet.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
-#include "planet.hpp"
 #include "ship.hpp"
 #include "fleet.hpp"
+#include "planet.hpp"
+#include "mothership.cpp"
+#include "player.hpp"
 
 #define FLEET_SPEED 10  // TODO: figure out the actual fleet speed
 
@@ -17,24 +19,26 @@ Fleet::Fleet(int quantity, Planet &origin, Planet &destination, int launchTime, 
 }
 
 
-int get_x(int t) { 
+int Fleet::get_x(int t) { 
   float p = (float)(t-startTime)/(float)(endTime-startTime);  // p: percent there
-  return (int)(orig.get_x()*(1-p) + dest.get_x()* p));
+  return (int)(orig.get_x()*(1-p) + dest.get_x()* p);
 }
 
-int get_y(int t) { 
+int Fleet::get_y(int t) { 
   float p = (float)(t-startTime)/(float)(endTime-startTime);  // p: percent there
-  return (int)(orig.get_y()*(1-p) + dest.get_y()* p));
+  return (int)(orig.get_y()*(1-p) + dest.get_y()* p);
 }
 
 
 int Fleet::update(int viewerX, int viewerY, int gameTime, int frameTime) {
-  float dist = hypot(get_x(gameTime)-owner.x, get_y(gameTime)-owner.y); // the distance to the "time immume" fleet
+  float dist = hypot(get_x(gameTime)-((owner.get_moship())->get_x()), get_y(gameTime)-(owner.get_moship())->get_y()); // the distance to the "time immume" fleet
   int t = gameTime - (int)(dist/LIGHTSPEED);  // the time of the viewed fleet
   screenX = get_x(t);
   screenY = get_y(t);
   
-  
+  for (int i=0; i<ships.size(); ++i) {
+    
+  }
   
   float p = (float)(t-startTime)/(float)(endTime-startTime);  // p: percent there
   if (p<0.f) return -1;
