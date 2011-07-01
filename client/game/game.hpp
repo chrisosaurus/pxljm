41ce4381577a4151f4888606a7fa3a26347e665a
TrueShiftBlue <TrueShiftBlue@gmail.com>
2011-07-02 03:00:42 +1200

    Fix ClientGame header, both in launch_fleet parameters and in indentation

--- client/game/game.hpp
+++ client/game/game.hpp
@@ -1,22 +1,22 @@
 class ClientGame {
-      public:
-        ClientGame();
-
-        ~ClientGame();
-
-        /**
-         * Send a fleet of the specified amount from the source planet, heading towards a destination planet.
-         */
-        void launch_fleet(Planet &src, Planet &dest, int count);
-
-        /**
-         * Find a planet based on id
-         */
-        Planet *planet_from_id(int id);
-
-        /**
-         * Find the closest planet to a specified location.
-         * If limit is specified, do not return a Planet further away than the squareroot of this.
-         */
-        Planet *find_nearest_planet(int x, int y, int limit = 0);
+public:
+    ClientGame();
+    
+    ~ClientGame();
+    
+    /**
+     * Send a fleet of the specified amount from the source planet, heading towards a destination planet.
+     */
+    void launch_fleet(Planet &src, Planet &dest);
+    
+    /**
+     * Find a planet based on id
+     */
+    Planet *planet_from_id(int id);
+    
+    /**
+     * Find the closest planet to a specified location.
+     * If limit is specified, do not return a Planet further away than the squareroot of this.
+     */
+    Planet *find_nearest_planet(int x, int y, int limit = 0);
 };
