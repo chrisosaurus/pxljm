57d1458ef637ed9c2d9d430a156403c8e94e518d
Chris Hall <followingthepath@gmail.com>
2011-07-01 18:56:39 +1200

    added to interface and dumb impl

---
+++ client/graphics/graphics.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <graphics.hpp>
+
+Graphics::Graphics(???){
+
+}
+
+void Graphics::draw(std::vector<Planet> planets, std::vector<Moship> moships, std::vector<???> ???){
+
+}
+
+void Graphics::draw_string(std::string st){
+
+}
--- client/graphics/graphics.hpp
+++ client/graphics/graphics.hpp
@@ -2,4 +2,5 @@ class Graphics{
   public:
     Graphics(???);
     void draw(std::vector<Planet> planets, std::vector<Moship> moships, std::vector<SpaceyShipyThings> SST);
+    void draw_string(std::string st);
 };
