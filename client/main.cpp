2ca0948fe75ee6b1743dece47a5f163b85ab7c65
Chris Hall <followingthepath@gmail.com>
2011-07-02 03:49:49 +1200

    added dull and mostly empty main

---
+++ client/main.cpp
@@ -0,0 +1,20 @@
+// oh god, it burns, IT BURNS!!!!
+
+// stuff for networking
+#include "networking/client.hpp"
+
+// stuff for interfacing.hpp
+#include "game/ship.hpp"
+#include "game/fleet.hpp"
+#include "game/planet.hpp"
+#include "game/game.hpp"
+#include "game/mothership.hpp"
+#include "game/player.hpp"
+#include "interfacing/interfacing.hpp"
+
+
+int main(){
+  //make a game, player and then feed them into Interface, then go!
+  // TODO
+  // also need to get networking involved somehow
+}
