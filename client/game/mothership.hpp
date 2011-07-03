d1d6138349198825c46022d2dfe5901cb614b189
TrueShiftBlue <TrueShiftBlue@gmail.com>
2011-07-03 12:27:43 +1200

    Force small mothership radius

--- client/game/mothership.hpp
+++ client/game/mothership.hpp
@@ -12,7 +12,7 @@ public:
     Mothership(int posx, int posy, int vid, Player *vowner);
 
     ~Mothership();
-
+    int get_radius() const { return 30; }
     int get_rotation() const { return rotation; }
 
     void draw(sf::RenderWindow &window, const sf::Color &c) const;
